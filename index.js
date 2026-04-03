d4a82f5c8f2cdac0cb1f9c9363700270ec7e0bd0
Mark Opondo <markopondoi.t@gmail.com>
2026-04-03 13:28:20 +0300

    pass tests

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0
 
+  return (passwordAttempt) => {
+      if (passwordAttempt === userInfo.password) {
+        return "Login successful"
+      }
+      attemptCount++
+
+      if (attemptCount > 3) {
+        return "Account locked due to too many failed login attempts"
+      }
+
+      return `Attempt ${attemptCount}: Login failed`
+  }
+}
 
 
 module.exports = {
